addf7e36abb9d0cf56806ef8d6d8089050ef70c7
Hasan Mahfoodh <hasoony201@gmail.com>
2026-02-06 17:02:16 +0300

    fix putting pics in the page issue

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const btn6by6 = document.querySelector("#btn-6-by-6")
 const btn8by8 = document.querySelector("#btn-8-by-8")
 
 let cardsContainer = document.querySelector(".four-by-four-container")
-let firstHalfCards = document.querySelectorAll(".first-half")
+let cards = document.querySelectorAll(".square")
+//let firstHalfCards = document.querySelectorAll(".square")
 let secondHalfCards = document.querySelectorAll(".second-half")
 let randomIndex
 let fruitPicture
 
-let originalPicsArray = [
+let picsArray = [
+  "4-by-4-imgs/apple.png",
+  "4-by-4-imgs/banana.png",
+  "4-by-4-imgs/berry.png",
+  "4-by-4-imgs/watermelon.png",
+  "4-by-4-imgs/kiwi.png",
+  "4-by-4-imgs/mango.png",
+  "4-by-4-imgs/strawberry.png",
+  "4-by-4-imgs/pineapple.png",
   "4-by-4-imgs/apple.png",
   "4-by-4-imgs/banana.png",
   "4-by-4-imgs/berry.png",
@@ -19,25 +28,43 @@ let originalPicsArray = [
   "4-by-4-imgs/pineapple.png",
 ]
 
-let firstHalfArray = [...originalPicsArray]
-let secondHalfArray = [...originalPicsArray]
+let halfPicsArr = [
+  "4-by-4-imgs/apple.png",
+  "4-by-4-imgs/banana.png",
+  "4-by-4-imgs/berry.png",
+  "4-by-4-imgs/watermelon.png",
+  "4-by-4-imgs/kiwi.png",
+  "4-by-4-imgs/mango.png",
+  "4-by-4-imgs/strawberry.png",
+  "4-by-4-imgs/pineapple.png",
+]
+
+let clonedArray = [...picsArray]
+//let secondHalfArray = [...originalPicsArray]
 
 window.addEventListener("load", () => {
-  firstHalfCards.forEach((card) => {
-    randomIndex = Math.floor(Math.random() * firstHalfArray.length)
+  cards.forEach((card) => {
+    randomIndex = Math.floor(Math.random() * clonedArray.length)
     fruitPicture = document.createElement("img")
-    fruitPicture.setAttribute("src", firstHalfArray[randomIndex])
+    fruitPicture.setAttribute("src", clonedArray[randomIndex])
     fruitPicture.style.width = "150px"
-    firstHalfArray.splice(randomIndex, 1)
-    card.appendChild(fruitPicture)
-  })
-
-  secondHalfCards.forEach((card) => {
-    randomIndex = Math.floor(Math.random() * secondHalfArray.length)
-    fruitPicture = document.createElement("img")
-    fruitPicture.setAttribute("src", secondHalfArray[randomIndex])
-    fruitPicture.style.width = "150px"
-    secondHalfArray.splice(randomIndex, 1)
+    clonedArray.splice(randomIndex, 1)
     card.appendChild(fruitPicture)
   })
 })
+
+/* let counter = 0
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    if (counter === 0 && ) {
+      console.log(card)
+      counter++
+    } else if (counter === 1) {
+      console.log("hehe")
+      //counter++
+    } else {
+      counter = 0
+    }
+  })
+}) */
