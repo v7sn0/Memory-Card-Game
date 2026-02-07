cb93038626db67b6e55077e187679712ce28e5ab
Hasan Mahfoodh <hasoony201@gmail.com>
2026-02-07 14:29:08 +0300

    add inital version of win condition functionality and fix lose condition functionality + improve data flow in the code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btn6by6 = document.querySelector("#btn-6-by-6")
 const btn8by8 = document.querySelector("#btn-8-by-8")
 
 let cardsContainer = document.querySelector(".four-by-four-container")
-let cards = document.querySelectorAll(".square")
+let cards = document.querySelectorAll(".not-selected") /////
 
 let randomIndex
 let fruitPicture
@@ -17,17 +17,6 @@ let picsArray = [
   "4-by-4-imgs/mango.png",
   "4-by-4-imgs/strawberry.png",
   "4-by-4-imgs/pineapple.png",
-  "4-by-4-imgs/apple.png",
-  "4-by-4-imgs/banana.png",
-  "4-by-4-imgs/berry.png",
-  "4-by-4-imgs/watermelon.png",
-  "4-by-4-imgs/kiwi.png",
-  "4-by-4-imgs/mango.png",
-  "4-by-4-imgs/strawberry.png",
-  "4-by-4-imgs/pineapple.png",
-]
-
-let halfPicsArr = [
   "4-by-4-imgs/apple.png",
   "4-by-4-imgs/banana.png",
   "4-by-4-imgs/berry.png",
@@ -53,6 +42,7 @@ window.addEventListener("load", () => {
 
 let counter = 0
 let chanceCounter = 0
+let matchCounter = 0
 let firstSelectedCard
 let secondSelectedCard
 let firstPic
@@ -60,46 +50,48 @@ let secondPic
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
-    card.classList.add("selected")
     if (
       counter === 0 &&
-      card.classList.contains("selected") &&
-      card.classList.contains("square")
+      card.classList.contains("not-selected") &&
+      chanceCounter <= 2
     ) {
       firstSelectedCard = card
       firstPic = card.innerHTML
-      firstSelectedCard.classList.remove("square")
+      firstSelectedCard.classList.remove("not-selected")
+      card.classList.add("selected")
       console.log(firstPic)
       counter++
     } else if (
       counter === 1 &&
-      card.classList.contains("selected") &&
-      card.classList.contains("square")
+      card.classList.contains("not-selected") &&
+      chanceCounter <= 2
     ) {
       secondSelectedCard = card
       secondPic = card.innerHTML
-      secondSelectedCard.classList.remove("square")
+      secondSelectedCard.classList.remove("not-selected")
+      card.classList.add("selected")
       console.log(secondPic)
       counter++
       if (firstPic === secondPic) {
-        console.log("reached")
-        firstSelectedCard.classList.remove("not-selected")
-        secondSelectedCard.classList.remove("not-selected")
-        firstSelectedCard.classList.remove("square")
-        secondSelectedCard.classList.remove("square")
         console.log(secondSelectedCard.classList)
+        matchCounter++
         counter = 0
       } else {
-        firstSelectedCard.classList.add("not-selected")
-        secondSelectedCard.classList.add("not-selected")
-        firstSelectedCard.classList.remove("selected")
-        secondSelectedCard.classList.remove("selected")
-        counter = 0
-        chanceCounter++
+        setTimeout(() => {
+          firstSelectedCard.classList.add("not-selected")
+          secondSelectedCard.classList.add("not-selected")
+          firstSelectedCard.classList.remove("selected")
+          secondSelectedCard.classList.remove("selected")
+          counter = 0
+          chanceCounter++
+        }, 1000)
       }
-      if (chanceCounter === 3) {
-        console.log("you lost")
-      }
+    } else if (chanceCounter === 3) {
+      console.log("lost")
+    }
+
+    if (matchCounter === 8) {
+      console.log("you won")
     }
   })
 })
